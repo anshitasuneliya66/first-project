d3ec9b099e55dd15bd0d68bb41c9808f626c2d30
Anshita Suneliya <suneliyaanshita@gmail.com>
2026-02-21 19:43:46 +0530

    Add files via upload

---
+++ 2nd program.py
@@ -0,0 +1,25 @@
+# str1 = "This is a python.\n We code in vs studio."
+# print(str1)
+"""
+str1 = "My"
+str2 = "name"
+final_str = str1+str2
+print(final_str)
+"""
+
+# str = "BEST COLLEGE"
+# ch = str[-4:-1]
+# print(ch)
+
+# str = "I am studying in best college"
+# print(str.capitalize())
+
+# print(str.replace("o","a"))
+
+# print(str.find("d"))
+# print(str.count("e"))
+
+
+#PRACTICE QUESTION
+NAME = input("Enter user's name:")
+print(len(NAME))
